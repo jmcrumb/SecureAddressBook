4104345a9d0a8bf9521145767671215e3f92d799
Miles Maloney <49667452+fartidiot@users.noreply.github.com>
2020-04-25 11:45:23 -0700

    Added constructors, toString, and fields
    Constructors take the same parameters; I merged them, but left both constructors in for now

--- src/com/AddressBook/UserEntry/UserEntry.java
+++ src/com/AddressBook/UserEntry/UserEntry.java
@@ -2,9 +2,40 @@
   * Title:          com.AddressBook
   * Authors:        Miles Maloney, Caden Keese, Kanan Boubion, Maxon Crumb, Scott Spinali
   * Last Modified:  4/22/20
-  * Description:
+  * Description: The UserEntry class holds all of the fields , constructors, and a toString method for each user's login data
   * */
  package com.AddressBook.UserEntry;
 
  public class UserEntry {
+    //fields
+    final String userid;
+    final String password;
+    private static boolean needsPWChange;
+
+    public UserEntry(String userid)
+    {
+        this.userid = userid;
+        needsPWChange = True; //flag to mark that the user must change passwords on next login
+    }
+
+    public UserEntry(String userEntryString)    //uncertain about this: both constructors just take strings
+    {
+        String fields[] = userEntryString.split(".");
+        if (fields.length == 1)         //merges the two constructors
+        {
+            this.userid = userid;
+            needsPWChange = true;
+        }
+        else
+        {
+            this.userid = fields[0];
+            this.password = fields[1];
+        }
+    }
+
+    public String toString()
+    {
+        String userEntry = this.userid + "." + this.password;  //this should either include the password or the fromString constructor should be removed
+        return userEntry;
+    }
  }
