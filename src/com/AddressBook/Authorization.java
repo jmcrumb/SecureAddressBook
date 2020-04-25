54e023fa82ca448f37f7f5594ca20734945adb6a
Maxon Crumb <jmcrumb3@gmail.com>
2020-04-24 18:51:18 -0700

    Authorization implemented

--- src/com/AddressBook/Authorization.java
+++ src/com/AddressBook/Authorization.java
@@ -6,5 +6,19 @@
   * */
  package com.AddressBook;
 
+ import com.AddressBook.User;
+
  public class Authorization {
+     
+    /**
+     * This function is used to make sure whoever is using the system is authorized 
+     * to use a command before it is executed.
+     * 
+     * @param command The command to authorize
+     * @return a boolean that is false if authorization fails and true if authorization 
+     * is successful
+     */
+    public static boolean verify(Command command) {
+        return User.getInstance().getAuthorization() == command.authRequirement;
+    }
  }
