6221216c5720c88ca102c448ce6aa4096b723bae
Miles Maloney <49667452+fartidiot@users.noreply.github.com>
2020-04-25 12:14:39 -0700

    Added some code *WIP

--- src/com/AddressBook/Command/DeleteUser.java
+++ src/com/AddressBook/Command/DeleteUser.java
@@ -2,9 +2,37 @@
   * Title:          com.AddressBook.Command
   * Authors:        Miles Maloney, Caden Keese, Kanan Boubion, Maxon Crumb, Scott Spinali
   * Last Modified:  4/22/20
-  * Description:
+  * Description: DeleteUser deletes a user from the address book device
   * */
  package com.AddressBook.Command;
 
  public class DeleteUser {
+    private String userid;
+  
+    public DeleteUser(String userid)
+    {
+        this.userid = userid;
+    }
+
+    public String execute() throws CommandException
+    {
+        currAuth = verify(this);
+        if(currAuth)
+        {
+            if(exists(this.userid))
+            {
+                userToDelete = new UserEntry(this.userid); //need decent way to remove user from user database
+                addressDatabase.set(userToDelete)
+            }
+            if(!exists(this.userid))
+            {
+                sendResponse("User not found.\n");
+            }
+            return authorizedCode;
+        }
+        else
+        {
+            return unauthorizedCode;
+        }
+    }
  }
