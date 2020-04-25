b1722421c072d8722d6d347f3b22c11ca7636fe8
Miles Maloney <49667452+fartidiot@users.noreply.github.com>
2020-04-25 11:42:29 -0700

    Edited constructor/ fields

--- src/com/AddressBook/UserEntry/AdminEntry.java
+++ src/com/AddressBook/UserEntry/AdminEntry.java
@@ -2,7 +2,7 @@
   * Title:          com.AddressBook.UserEntry
   * Authors:        Miles Maloney, Caden Keese
   * Last Modified:  4/24/20
-  * Description:
+  * Description: AdminEntry class intended to hold information for the admin account of the address book
   * */
  package com.AddressBook.UserEntry;
 
