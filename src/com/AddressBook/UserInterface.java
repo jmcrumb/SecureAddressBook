8b016515236d6cc2d9cd52447df04ac38facbbe4
Miles Maloney <49667452+fartidiot@users.noreply.github.com>
2020-04-25 18:11:28 -0700

    Added my UserInterface
    Imported my code. Added all functionalities adding the command parser.

--- src/com/AddressBook/UserInterface.java
+++ src/com/AddressBook/UserInterface.java
@@ -12,69 +12,233 @@
  import java.util.regex.Pattern;
 
  public class UserInterface {
-     private final Parser parser;
-     private Scanner sc;
+    private String commands[] = new String[13];
+    public UserInterface()         //initializes the commands array with all possible commands
+    {
+        commands[0] = "LIN";
+        commands[1] = "LOU";
+        commands[2] = "CHP";
+        commands[3] = "ADU";
+        commands[4] = "DEU";
+        commands[5] = "DAL";
+        commands[6] = "ADR";
+        commands[7] = "DER";
+        commands[8] = "EDR";
+        commands[9] = "RER";
+        commands[10] = "IMD";
+        commands[11] = "EXD";
+        commands[12] = "HLP";
+    }
 
-     public UserInterface(Parser parser) {
-         this.parser = parser;
-     }
+    public Command getNextCommand()
+    {
+        boolean commandFound = false;
+        Console cnsl = null;
+        try{
+            cnsl = System.console();
+            String pass = "";
+            while(!commandFound && cnsl != null)
+            {
+ 
+                String fullString = cnsl.readLine();   //recieves the next command line
+                String cmdString = fullString.substring(0,2); //recieves the first 3 characters of the command line (the command codes)
+                for(int i = 0; i < commands.length - 1; i++)
+                {
+                    if(cmdString.equalsIgnoreCase(commands[i])) //case: command is found (case insensitive)
+                    {
+                        commandFound = true;
+                        return commandParse(fullString);
+                    }
+                
+                }
+                sendResponse("Could not find command. Please try again or type 'help' for a list of commands\n"); //user inputs invalid command
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error getting next command.\n");
+        }
 
-     void sendResponse(String response) {
-         System.out.println(response);
-     }
+        return "error"; //the code should never reach this if it is working properly: this can be vetted in the app loop
+    }
 
-     public Command getNextCommand() {
-         Command command = null;
-         while (command == null) {
-             String raw = System.console().readLine();
-             if (!validateTotalCharSet(raw)) ;
-             String cmdStr = raw.substring(0, 3);
-             switch (cmdStr) {
-                 case "LIN":
-                     return command = Login(raw);
-                     break;
-                 case "LOU":
-                     return command = Logout(raw);
-                     break;
-                 case "CHP":
-                     return command = ChangePassword(raw);
-                     break;
-                 case "ADU":
-                     return command = AddUser(raw);
-                     break;
-                 case "DEU":
-                     return command = DeleteUser(raw);
-                     break;
-                 case "DAL":
-                     return command = DisplayAuditLog(raw);
-                     break;
-                 case "ADR":
-                     return command = AddRecord(raw);
-                     break;
-                 case "DER":
-                     return command = DeleteRecord(raw);
-                     break;
-                 case "EDR":
-                     return command = EditRecord(raw);
-                     break;
-                 case "RER":
-                     return command = ReadRecord(raw);
-                     break;
-                 case "IMD":
-                     return command = ImportDatabase(raw);
-                     break;
-                 case "EXD":
-                     return command = ExportDatabase(raw);
-                     break;
-                 case "HLP":
-                     return command = HLP(raw);
-                     break;
-                 default:
-                     break;
-             }
-         }
-         return null;
-     }
+    public void sendResponse(String response)
+    {
+        System.out.println(response);
+    }
+
+    public Command commandParse(String commandString)
+    {
+        String sub = commandString.substring(0,2);
+        switch(sub)
+        {
+            case "LIN": //username + ";" + plain text pw
+                String[] linargs = commandString.split(" ");   //*will add if args.length < 3
+                String linuser = UserInterface.removeBrackets(linargs[1]);
+                String linpw = UserInterface.removeBrackets(linargs[2]);
+                String lininput = linuser + ";" + linpw;
+                Login logIn = new Login(lininput);
+                return logIn;
+
+            case "LOU":
+                Logout logOut = new Logout();
+                return logOut;
+
+            case "CHP": //username + ";" + plain text pw
+                ChangePassword changePassword = null;
+                String[] chpargs = commandString.split(" ");
+                String oldpw = UserInterface.removeBrackets(chpargs[1]); // *need to verify current password (waiting to see how login recieves pw from hash)
+            
+                Console cnsl = null;
+                String newPw = "";
+                try{
+                    cnsl = System.console();
+                    boolean confirmed = false;
+                    while(!confirmed)
+                    {
+                        System.out.println("Please enter your new password");
+                        newPw = cnsl.readLine();
+                        System.out.println("Please confirm your new password");
+                        if(cnsl.readLine().equals(newPw))
+                        {
+                            confirmed = true;
+                        }
+                    }
+                    String chpuser = null;//*User.getInstance().getID();
+                    String chpinput = chpuser + ";" + newPw;
+                    changePassword = new ChangePassword(chpinput);
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Error parsing command through console.\n");
+                }
+                return changePassword;
+
+            case "ADU":
+                String aduinput = parseBasicCommand(commandString);
+                AddUser addUser = new AddUser(aduinput);
+                return addUser;
+
+            case "DEU":
+                String deuinput = parseBasicCommand(commandString);
+                DeleteUser deleteUser = new DeleteUser(deuinput);
+                return deleteUser;
+            case "DAL":
+                String dalinput = parseBasicCommand(commandString);
+                DisplayAuditLog displayAuditLog = new DisplayAuditLog(dalinput);
+                return displayAuditLog;
+
+            case "ADR":
+                String adrinput = parseBasicCommand(commandString);
+                AddRecord addRecord = new AddRecord(adrinput);
+                return deleteUser;
+            
+            case "DER":
+                String derinput = parseBasicCommand(commandString);
+                DeleteRecord deleteRecord = new DeleteRecord(derinput);
+                return deleteRecord;
+
+            case "EDR":
+                String edrinput = UserInterface.parseBasicCommand(commandString);
+                EditRecord editRecord = new EditRecord(edrinput);
+                return editRecord;
+
+            case "RER": 
+                String rerinput = UserInterface.parseBasicCommand(commandString);
+                ReadRecord readRecord = new ReadRecord(rerinput);
+                return readRecord;
+
+            case "IMD":
+                String imdinput = UserInterface.parseBasicCommand(commandString);
+                ImportDatabase importDatabase = new ImportDatabase(imdinput);
+                return readRecord;
+            
+            case "EXD":
+                String exdinput = parseBasicCommand(commandString);
+                ExportDatabase exportDatabase = new ExportDatabase(exdinput);
+                return exportDatabase;
+
+            case "HLP":
+                String hlpinput = parseBasicCommand(commandString);
+                UserInterface.helpHandler(hlpinput);
+        }
+
+    }
+
+    public static String removeBrackets(String s)   //function to remove brackets from original syntax
+    {
+        s.replace("<","");
+        s.replace(">","");
+        return s;
+    }
+
+    public static String parseBasicCommand(String cmdString)    //function to get the input string of the command easily
+    {
+        String[] cmdargs = cmdString.split(" ");
+        return cmdargs[1];
+    }
+
+    public static void helpHandler(String hlpinput)
+    {
+        if(hlpinput == null)
+        {
+            System.out.println("LIN <userID> <password>\n");
+            System.out.println("LOU\n");
+            System.out.println("CHP <old password>\n");
+            System.out.println("ADU <userID>\n");
+            System.out.println("DEU <userID>\n");
+            System.out.println("DAL <userID>\n");
+            System.out.println("ADR <recordID> [<field1 = value1> <field2 = value2>...]\n"); //**NEED TO DEAL WITH THIS SYNTAX
+            System.out.println("DER <recordID>\n");
+            System.out.println("EDR <recordID> [<field1 = value1> <field2 = value2>...]\n");
+            System.out.println("RER <recordID> [<fieldname> ...]\n");
+            System.out.println("IMD <Input_File>\n");
+            System.out.println("EXD <Output_File>\n");
+            System.out.println("HLP [<command name>]\n"); 
+        }
+        switch(hlpinput)
+        {
+            case "LIN":
+                System.out.println("LIN <userID> <password>\n");
+                break;
+            case "LOU":
+                System.out.println("LOU\n");
+                break;
+            case "CHP":
+                System.out.println("CHP <old password>\n");
+                break;
+            case "ADU":
+                System.out.println("ADU <userID>\n");
+                break;
+            case "DEU":
+                System.out.println("DEU <userID>\n");
+                break;
+            case "DAL":
+                System.out.println("DAL <userID>\n");
+                break;
+            case "ADR":
+                System.out.println("ADR <recordID> [<field1 = value1> <field2 = value2>...]\n"); //**NEED TO DEAL WITH THIS SYNTAX
+                break;
+            case "DER":
+                System.out.println("DER <recordID>\n");
+                break;
+            case "EDR":
+                System.out.println("EDR <recordID> [<field1 = value1> <field2 = value2>...]\n");
+                break;
+            case "RER":
+                System.out.println("RER <recordID> [<fieldname> ...]\n");
+                break;
+            case "IMD":
+                System.out.println("IMD <Input_File>\n");
+                break;
+            case "EXD":
+                System.out.println("EXD <Output_File>\n");
+                break;
+            case "HLP":
+                System.out.println("HLP [<command name>]\n"); 
+                break;
+        }
+    }
 
 //     private boolean authorizeForChangePassword(){
 ////         System.console().printf("Current Password?\n");
