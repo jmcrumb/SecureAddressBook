db47d3b2809d997c28bdac3ab220706b9b58da8e
Miles Maloney <49667452+fartidiot@users.noreply.github.com>
2020-04-25 18:27:50 -0700

    Added authlevel, debugging, and removing bad code
    Many compiler issues were fixed and commented out some useless/bugged code

--- src/com/AddressBook/UserEntry/UserEntry.java
+++ src/com/AddressBook/UserEntry/UserEntry.java
@@ -6,36 +6,58 @@
   * */
  package com.AddressBook.UserEntry;
 
- public class UserEntry {
-    //fields
+public class UserEntry
+{
     final String userid;
-    final String password;
-    private static boolean needsPWChange;
+    final String pwhash;
+    final int authLevel;
 
-    public UserEntry(String userid)
+    public UserEntry(String userid, String pwhash, int authLevel)
     {
         this.userid = userid;
-        needsPWChange = True; //flag to mark that the user must change passwords on next login
+        this.pwhash = pwhash;
+        this.authLevel = authLevel;
     }
 
     public UserEntry(String userEntryString)    //uncertain about this: both constructors just take strings
     {
-        String fields[] = userEntryString.split(".");
-        if (fields.length == 1)         //merges the two constructors
+        String fields[] = userEntryString.split(";");
+        if(fields.length < 2)
         {
-            this.userid = userid;
-            needsPWChange = true;
+            sendResponse("invalid user entry string \n");
+            this.userid = null;
+            this.pwhash = null;
         }
-        else
+ 
+//       else if(fields.length > 2)
+//        {
+//            int counter = 0;
+//            String incuserid = "";
+//            String pass = "";
+//           while(!exists(incuserid))   //gets the userid if it contains ; characters by checking whether the user exists at each stage ***could give a shorter userid than what is intended if multiple ~ characters are involved and one userid is a substring of another
+//            {
+//                incuserid += fields[counter];
+//                counter++;
+//            }
+//            counter++;  //starts the password after the userid
+//            while(counter < fields.length - 1)    //gets the password if it contains ; characters by taking the rest of the strings in the array and concatenating them together
+//            {
+//                pass += fields[counter];
+//               counter++;
+//          }
+//         this.userid = incuserid;
+//         this.pwhash = pass; 
+//        }
+        else    //fields.length = 2
         {
             this.userid = fields[0];
-            this.password = fields[1];
+            this.pwhash = fields[1];
         }
     }
 
     public String toString()
     {
-        String userEntry = this.userid + "." + this.password;  //this should either include the password or the fromString constructor should be removed
+        String userEntry = this.userid + ";" + this.pwhash;  //this should either include the password or the fromString constructor should be removed
         return userEntry;
     }
- }
+}
