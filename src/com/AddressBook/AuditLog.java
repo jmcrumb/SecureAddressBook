d6e5de161245da935975199e566db1e0ee523048
Caden Keese <caden@keese.dev>
2020-04-27 11:46:57 -0700

    auditlog

--- src/com/AddressBook/AuditLog.java
+++ src/com/AddressBook/AuditLog.java
@@ -8,8 +8,12 @@
  import java.io.File;
  import java.io.FileWriter;
  import java.io.IOException;
+ import java.nio.file.Files;
+ import java.nio.file.Paths;
 
-import com.AddressBook.Database.UserDatabase;
+ import com.AddressBook.Command.Command;
+ import com.AddressBook.Database.UserDatabase;
+ import com.AddressBook.UserEntry.UserEntry;
 
  public class AuditLog {
 
@@ -26,30 +30,30 @@ import com.AddressBook.Database.UserDatabase;
     //Logs the command of all user
     //figure out where to put the file, make sure in same directory as program
     //Format is command(input);authorization(Yes/No)
-    void logCommand(Command command, boolean authorized){  
-        try{
-            if(command == null){
+    void logCommand(Command command, boolean authorized){
+        try {
+            if (command == null) {
                 return;
             }
-            if(!logHistory.exists()){
+            if (!logHistory.exists()) {
                 logHistory = new File("logHistory.txt");
             }
-            
-            String authorization;
-            if(authorized){
-                authorization = "Yes";
-            }
-            else{
-                authorization = "No";
-            }
+
 
             //Encrypt so only admin would be able to access eventually
-            String adminPassword = UserDatabase.get(admin).password; 
-            
-            FileWriter FW = new FileWriter("logHistory.txt");
-            //Can someone check if this is correct
-            FW.write(Encyption.encrypt(command.input,adminPassword) + ";" + Encryption.encrypt(authorization));
-            FW.close();
+            UserEntry adm = UserDatabase.getInstance().get("admin");
+            String adminPassword;
+            if (adm != null) {
+                adminPassword = adm.passwordHash;
+            } else {
+                throw new RuntimeException("Admin doesn't exist");
+            }
+            if (authorized && command.getAuthorizedCode() != null) {
+                Files.writeString(Paths.get("logHistory.txt"), "" + User.getInstance().getUserId() + ";" + command.getAuthorizedCode());
+            } else if (!authorized && command.getUnauthorizedCode() != null) {
+                Files.writeString(Paths.get("logHistory.txt"), "" + User.getInstance().getUserId() + ";" + command.getUnauthorizedCode());
+
+            }
         }
 
         catch (IOException e){
