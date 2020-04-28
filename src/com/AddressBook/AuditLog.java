1f826794eca2ba8c66f65a43d5830a424930cea4
Maxon Crumb <jmcrumb3@gmail.com>
2020-04-27 21:57:27 -0700

    delete record patched

--- src/com/AddressBook/AuditLog.java
+++ src/com/AddressBook/AuditLog.java
@@ -5,8 +5,6 @@
   * Description:
   * */
  package com.AddressBook;
- import java.io.File;
- import java.io.FileWriter;
  import java.io.IOException;
  import java.nio.file.Files;
  import java.nio.file.Paths;
